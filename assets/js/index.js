271262afdf694d5dd50914b290b1666f2635b685
ChetanBudhathoki <chetanbudhathoki1028@gmail.com>
2022-12-21 17:17:37 +0545

    dropdown arrow and up arrow made functional

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,36 +6,57 @@ let position = -80;
 // selectors
 
 const menu = document.querySelector(".menubar");
-const dropDownContainer = document.querySelector(".dropdown_container");
+// const dropDownContainer = document.querySelector(".dropdown_container");
 
+const dropDownContainer = document.querySelector(".navbar_middle");
 
-//html element creation 
-const menuBtn=document.createElement("img");
-menuBtn.classList.add('menu_icon');
-menuBtn.src="./assets/images/menu.svg";
+const body = document.querySelector("body");
+var navbar = document.querySelector(".navbar");
+var sticky = navbar.offsetHeight;
+var herocontainer = document.querySelector(".hero_container");
+
+const downangle = document.getElementById("down_angle");
+const tohomearrow=document.querySelector(".to_home_arrow");
+
+//html element creation
+const menuBtn = document.createElement("img");
+menuBtn.classList.add("menu_icon");
+menuBtn.src = "./assets/images/menu.svg";
 menu.appendChild(menuBtn);
 
-
 // eventlisteners
-
+downangle.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollBy(0,1000);
+});
+tohomearrow.addEventListener("click", (e) => {
+  // e.preventDefault();
+  console.log('HELLOW upangle');
+  // window.scrollBy(0,10);
+});
 menu.addEventListener("click", (e) => {
   e.preventDefault();
   e.stopPropagation();
   console.log(e.target);
   console.log("hellow there");
-  isDrawerOpen?menuBtn.src="./assets/images/menu.svg":menuBtn.src="./assets/images/cancelmenu.svg";
+  isDrawerOpen
+    ? (menuBtn.src = "./assets/images/menu.svg")
+    : (menuBtn.src = "./assets/images/cancelmenu.svg");
+  isDrawerOpen
+    ? (body.style.overflow = "unset")
+    : (body.style.overflow = "hidden");
   isDrawerOpen = !isDrawerOpen;
   let test = setInterval(() => {
     slideDrawer();
-    dropDownContainer.style.right = position + "vw";
+    dropDownContainer.style.top = position + "%";
     // console.log("POSITON is :", position);
 
-    if (position >= 0 || position <= -80) {
+    if (position >= 15 || position <= -85) {
       clearInterval(test);
       console.log("cleard");
-      isDrawerOpen ? position = 0 : position = -80;
+      isDrawerOpen ? (position = 0) : (position = -80);
     }
-  }, 5);
+  }, 1);
 });
 
 // functions
@@ -49,17 +70,29 @@ const slideDrawer = () => {
   }
 };
 
+window.onscroll = function () {
+  
+  myFunction();
+};
 
-
-window.onscroll = function() {myFunction()};
-
-var navbar = document.querySelector(".navbar");
-var sticky = navbar.offsetHeight;
 
 function myFunction() {
+  console.log(window.pageYOffset);
+  if(window.pageYOffset>=1){
+    downangle.style.display="none";
+    tohomearrow.style.display="flex";
+  }
+  else{
+    downangle.style.display="unset";
+    tohomearrow.style.display="none";
+  }
+  
+  
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
+    herocontainer.classList.add("hero_container_margin_top");
   } else {
     navbar.classList.remove("sticky");
+    herocontainer.classList.remove("hero_container_margin_top");
   }
 }
